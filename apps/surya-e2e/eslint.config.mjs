e9239fa8168ada7a5d38f375b4ffeb7ccc982e32
Vakadu Vinod <vakadu10@gmail.com>
2025-01-31 13:39:31 +0530

    new

--- apps/surya-e2e/eslint.config.mjs
+++ apps/surya-e2e/eslint.config.mjs
@@ -1,11 +1,12 @@
 import cypress from 'eslint-plugin-cypress/flat';
+
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  cypress.configs['recommended'],
-  ...baseConfig,
-  {
-    // Override or add rules here
-    rules: {},
-  },
+	cypress.configs['recommended'],
+	...baseConfig,
+	{
+		// Override or add rules here
+		rules: {},
+	},
 ];
