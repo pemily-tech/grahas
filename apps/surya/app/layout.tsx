72d44ef1b5503fba3a1dc1fbcf5dbcfedec8c5e3
Vakadu Vinod <vakadu10@gmail.com>
2025-02-01 21:05:45 +0530

    feat(fix): chaneg

--- apps/surya/app/layout.tsx
+++ apps/surya/app/layout.tsx
@@ -2,42 +2,8 @@ import '../assets/styles/globals.css';
 
 import { type PropsWithChildren } from 'react';
 import { cn } from '@grahas/utils';
-import { type Metadata } from 'next';
 
-import { fonts, siteConfig } from '../helpers';
-
-export const generateMetadata = (): Metadata => ({
-  metadataBase: new URL(siteConfig.url()),
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  keywords: siteConfig.keywords(),
-  robots: { index: true, follow: true },
-  icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
-  verification: {
-    // google: siteConfig.googleSiteVerificationId(),
-  },
-  openGraph: {
-    url: siteConfig.url(),
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: '/favicon/icon-512-maskable.png',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: '/favicon/icon-512-maskable.png',
-  },
-});
+import { fonts } from '../helpers';
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
